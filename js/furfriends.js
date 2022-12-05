1465973add9aefd0ca02ee8b46333a7603a62e69
carlykrenek <117031782+carlykrenek@users.noreply.github.com>
2022-12-04 19:14:00 -0600

    Add files via upload

--- js/furfriends.js
+++ js/furfriends.js
@@ -4,3 +4,40 @@ $(function() {
         $(this).parent().addClass("active");
     });
 });
+
+/*Contact Form*/
+
+// listen for form submission
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// submit form (MAIN)
+function submitForm(e) {
+    // prevent the default behaviour of submit
+    e.preventDefault();
+
+    // get values
+    var name = getInputVal('name');
+    var company = getInputVal('company');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+
+    // save message
+    saveMessage(name, company, email, phone, message);
+
+    // show alert on successful submition
+    document.querySelector('.alert').style.display = 'block';
+
+    // hide alert after 3 seconds (timeout)
+    setTimeout(function() {
+        document.querySelector('.alert').style.display = 'none';
+    }, 3000);
+
+    // clear form after submission
+    document.getElementById('contactForm').reset();
+}
+
+// get specific input value
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
